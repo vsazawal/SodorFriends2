0d3ef518f6b171a240cbad3ad21afe8b0ce4ed6f
Vibha <vibha.sazawal@gmail.com>
2016-04-13 14:33:47 -0400

    switching to recycler view

--- app/src/main/java/edu/umd/cs/sodorfriends2/TrainListActivity.java
+++ app/src/main/java/edu/umd/cs/sodorfriends2/TrainListActivity.java
@@ -1,18 +1,25 @@
 package edu.umd.cs.sodorfriends2;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class TrainListActivity extends AppCompatActivity
-        implements TrainListFragment.Callbacks {
+public class TrainListActivity extends AppCompatActivity {
 
     private SodorDB mDatabase;
     private ArrayList<Train> mTrains;
@@ -25,15 +32,65 @@ public class TrainListActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
+
         mDatabase = new SodorDB(getApplicationContext());
         mTrains = mDatabase.getTrains();
+        RecyclerView list = findViewById(R.id.trains_list);
+        list.setAdapter(new SimpleItemRecyclerViewAdapter(new ContinentNameList()));
+        //setListAdapter(new TrainArrayAdapter(getContext(), activity.getTrains()));
     }
 
     public ArrayList<Train> getTrains() {
         return mTrains;
     }
+    public class TrainRecyclerViewAdapter
+            extends RecyclerView.Adapter<TrainRecyclerViewAdapter.ViewHolder> {
 
-    public void onItemSelected(String id) {
-        // do nothing for now
+
+        public TrainRecyclerViewAdapter() {
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.train_list_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            final Train train = mTrains.get(position);
+            holder.getNameView().setText(mValues.get(position).getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTrains.size();
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+            private final CardView mCardView;
+            private final ImageView mImage;
+            private final TextView mName;
+            private final TextView mNumber;
+
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mCardView = (CardView) itemView.findViewById(R.id.card_view);
+                mImage = (ImageView) itemView.findViewById(R.id.train_image);
+                mName = (TextView) itemView.findViewById(R.id.train_name);
+                mNumber = (TextView) itemView.findViewById(R.id.train_image);
+            }
+
+            @Override
+            public String toString() {
+                String toreturn = super.toString() + " '" + mName.getText() + "'";
+                return toreturn;
+                }
+            }
+
     }
 }
